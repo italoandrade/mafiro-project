e9ddbbe1afca20a92916364a9830f52f999cf227
Ítalo Andrade <andraditalo@gmail.com>
2017-01-28 21:46:46 -0200

    Se tiver um header, o body vai ganhar uma margem no topo para que o conteúdo não fica atrás do header

--- mafiro-project/src/structure/header/header.js
+++ mafiro-project/src/structure/header/header.js
@@ -8,17 +8,25 @@
         console.log('- - Loading component "header"');
 
         window.addEventListener('scroll', () => {
-            const $elements = mafiro.element('.mi-header');
-
-            mafiro.each($elements, (i, $element) => {
-                if (window.scrollY) {
-                    mafiro.class.add($element, 'scrolled');
-                } else {
-                    mafiro.class.remove($element, 'scrolled');
-                }
-            });
+            verifyScroll();
         });
 
+        verifyScroll();
+
         console.log('- - Component "header" loaded');
+    }
+
+    function verifyScroll() {
+        const $elements = mafiro.element('.mi-header');
+
+        mafiro.each($elements, (i, $element) => {
+            mafiro.class.add(mafiro.element('body')[0], 'with-header');
+
+            if (window.scrollY) {
+                mafiro.class.add($element, 'scrolled');
+            } else {
+                mafiro.class.remove($element, 'scrolled');
+            }
+        });
     }
 })();
