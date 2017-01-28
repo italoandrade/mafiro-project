6df453f8b0927df23f08106210dbdf62564b42bf
Ítalo Andrade <andraditalo@gmail.com>
2017-01-28 12:37:53 -0200

    Correção do animate, para aceitar vários parâmetros como a propriedade, o valor que começa, o valor que termina, quanto tempo irá durar, callback final e callback de ticks

--- mafiro-project/src/core/animate/animate.js
+++ mafiro-project/src/core/animate/animate.js
@@ -1,7 +1,29 @@
 (() => {
     mafiro.animate = animate;
 
-    function animate(callback, duration) {
+    function animate(object, property, start_value, end_value, time, end, tick) {
+        var frame_rate = 0.06; // 60 FPS
+        var frame = 0;
+        var delta = (end_value - start_value) / time / frame_rate;
+        const propWithPx = mafiro.style.propWithPx;
+        var handle = setInterval(function() {
+            frame++;
+            var value = start_value + delta * frame;
+
+            if (tick) tick(value);
+
+            const hasPx = propWithPx.indexOf(property) > -1;
+
+            object.style[property] = value + (hasPx ? "px" : '');
+            if (value == end_value) {
+                clearInterval(handle);
+
+                if (end) end();
+            }
+        }, 1 / frame_rate);
+    }
+
+    function animate2(config, callback) {
         let end;
 
         const interval = 33;
@@ -10,7 +32,7 @@
         const animation = setInterval(frame, interval);
 
         function frame() {
-            if (ticks  > duration / interval - 1) {
+            if (ticks  > config.in / interval - 1) {
                 clearInterval(animation);
 
                 if (end) end();
