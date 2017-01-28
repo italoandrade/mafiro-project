2e2f25a91866b7e5b91193effd27f80b630e6349
Ítalo Andrade <andraditalo@gmail.com>
2017-01-28 12:36:05 -0200

    Criação do componente card

---
+++ mafiro-project/src/components/card/card.js
@@ -0,0 +1,29 @@
+(() => {
+    mafiro.components.add({
+        name: 'card',
+        load: loadCard,
+        onWindowLoad: onWindowLoad
+    });
+
+    function loadCard() {
+        return {
+            on: (element) => {
+                mafiro.components.load('ripple').on(element);
+            }
+        };
+    }
+
+    function onWindowLoad() {
+        console.log('- - Loading component "card"');
+
+        const $cards = mafiro.element('.mi-card');
+
+        mafiro.each($cards, (i, $card) => {
+            if (!$card.disabled) {
+                mafiro.components.load('card').on($card);
+            }
+        });
+
+        console.log('- - Component "card" loaded');
+    }
+})();
