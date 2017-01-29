663f7c63951c6ec52326b96df6056cfb82a3fe69
Ítalo Andrade <andraditalo@gmail.com>
2017-01-29 19:43:25 -0200

    Implementando para o modo mobile, para esconder o header ao descer a tela e mostra-lo ao subir

--- mafiro-project/src/structure/header/header.js
+++ mafiro-project/src/structure/header/header.js
@@ -11,6 +11,33 @@
             verifyScroll();
         });
 
+        let ts;
+        window.addEventListener('touchstart', function (e){
+            ts = e.touches[0].clientY;
+        });
+
+        window.addEventListener('touchmove', function (e){
+            const $header = mafiro.element('.mi-header')[0];
+
+            let te = e.changedTouches[0].clientY;
+
+            let translate = ts - te;
+
+            translate = translate < -70 ? -70 : translate;
+            translate = translate > 70 ? 70 : translate;
+
+            if (ts > te+5){
+
+                mafiro.style.set($header, 'transform', 'translateY(-' + translate + 'px)');
+            }else if(ts < te-5){
+                const isAlreadyOnTop = mafiro.style.get($header, 'transform') != 'matrix(1, 0, 0, 1, 0, 0)';
+
+                if (isAlreadyOnTop) {
+                    mafiro.style.set($header, 'transform', 'translateY(' + (-70 - translate) + 'px)');
+                }
+            }
+        });
+
         mafiro.class.add(mafiro.element('body')[0], 'with-header');
 
         const $navButton = mafiro.element('.mi-nav-button')[0];
