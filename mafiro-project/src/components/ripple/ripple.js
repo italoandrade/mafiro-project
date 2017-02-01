6ef34e282132f689d4d8f8b6abc4f78dc6d6f50c
Ítalo Andrade <andraditalo@gmail.com>
2017-02-01 21:49:38 -0200

    Correção: Ripple atrapalhando o evento de click no iOS

--- mafiro-project/src/components/ripple/ripple.js
+++ mafiro-project/src/components/ripple/ripple.js
@@ -22,63 +22,65 @@
                 }
 
                 element.addEventListener('mousedown', (e) => {
-                    const elementWidth = parseInt(mafiro.style.get(element, 'width'));
-                    const elementHeight = parseInt(mafiro.style.get(element, 'height'));
+                    setTimeout(() => {
+                        const elementWidth = parseInt(mafiro.style.get(element, 'width'));
+                        const elementHeight = parseInt(mafiro.style.get(element, 'height'));
 
-                    mafiro.style.set($rippleContainerTemplateClone, 'border-radius', mafiro.style.get(element, 'border-radius'));
-                    $rippleContainerTemplateClone.style.width = elementWidth + 'px';
-                    $rippleContainerTemplateClone.style.height = elementHeight + 'px';
+                        mafiro.style.set($rippleContainerTemplateClone, 'border-radius', mafiro.style.get(element, 'border-radius'));
+                        $rippleContainerTemplateClone.style.width = elementWidth + 'px';
+                        $rippleContainerTemplateClone.style.height = elementHeight + 'px';
 
-                    element.appendChild($rippleContainerTemplateClone);
+                        element.appendChild($rippleContainerTemplateClone);
 
-                    const $rippleTemplateClone = $rippleTemplate.cloneNode(true);
+                        const $rippleTemplateClone = $rippleTemplate.cloneNode(true);
 
-                    const isElementHorizontal = elementWidth > elementHeight;
+                        const isElementHorizontal = elementWidth > elementHeight;
 
-                    let finalWidth;
-                    let finalHeight;
-                    let finalTop;
-                    let finalLeft;
+                        let finalWidth;
+                        let finalHeight;
+                        let finalTop;
+                        let finalLeft;
 
-                    if (isElementHorizontal) {
-                        finalWidth = elementWidth * 3;
-                        finalHeight = elementWidth * 3;
-                    } else {
-                        finalWidth = elementHeight * 3;
-                        finalHeight = elementHeight * 3;
-                    }
+                        if (isElementHorizontal) {
+                            finalWidth = elementWidth * 3;
+                            finalHeight = elementWidth * 3;
+                        } else {
+                            finalWidth = elementHeight * 3;
+                            finalHeight = elementHeight * 3;
+                        }
 
-                    finalTop = - (finalWidth / 2)/* + (elementHeight / 2)*/;
-                    finalLeft = - (finalWidth / 2)/* + (elementWidth / 2)*/;
+                        finalTop = - (finalWidth / 2)/* + (elementHeight / 2)*/;
+                        finalLeft = - (finalWidth / 2)/* + (elementWidth / 2)*/;
 
-                    mafiro.style.set($rippleTemplateClone, 'width', finalWidth);
-                    mafiro.style.set($rippleTemplateClone, 'height', finalHeight);
+                        mafiro.style.set($rippleTemplateClone, 'width', finalWidth);
+                        mafiro.style.set($rippleTemplateClone, 'height', finalHeight);
 
-                    const elementPos = mafiro.element.position(element);
-                    const mousePos = mafiro.mouse.position(e);
+                        const elementPos = mafiro.element.position(element);
+                        const mousePos = mafiro.mouse.position(e);
 
-                    const pos = {
-                        y: mousePos.y - elementPos.top,
-                        x: mousePos.x - elementPos.left
-                    };
+                        const pos = {
+                            y: mousePos.y - elementPos.top,
+                            x: mousePos.x - elementPos.left
+                        };
 
-                    if (mafiro.style.get(element, 'border-radius') === elementWidth / 2) {
-                        finalTop = finalTop + (elementWidth / 2);
-                        finalLeft = finalLeft + (elementWidth / 2);
-                    } else {
-                        finalTop = finalTop + pos.y;
-                        finalLeft = finalLeft + pos.x;
-                    }
+                        if (mafiro.style.get(element, 'border-radius') === elementWidth / 2) {
+                            finalTop = finalTop + (elementWidth / 2);
+                            finalLeft = finalLeft + (elementWidth / 2);
+                        } else {0
+                            finalTop = finalTop + pos.y;
+                            finalLeft = finalLeft + pos.x;
+                        }
 
-                    mafiro.style.set($rippleTemplateClone, 'top', finalTop);
-                    mafiro.style.set($rippleTemplateClone, 'left', finalLeft);
+                        mafiro.style.set($rippleTemplateClone, 'top', finalTop);
+                        mafiro.style.set($rippleTemplateClone, 'left', finalLeft);
 
-                    mafiro.element.prepend($rippleContainerTemplateClone, $rippleTemplateClone);
+                        mafiro.element.prepend($rippleContainerTemplateClone, $rippleTemplateClone);
 
-                    mafiro.class.add($rippleContainerTemplateClone, 'pressed');
+                        mafiro.class.add($rippleContainerTemplateClone, 'pressed');
 
-                    mafiro.animate($rippleContainerTemplateClone, 'border-spacing', 0, 1, 300, null, (tick) => {
-                        mafiro.style.set($rippleTemplateClone, 'transform', 'scale(' + tick + ')');
+                        mafiro.animate($rippleContainerTemplateClone, 'border-spacing', 0, 1, 300, null, (tick) => {
+                            mafiro.style.set($rippleTemplateClone, 'transform', 'scale(' + tick + ')');
+                        });
                     });
                 });
 
