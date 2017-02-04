85d06477938730b13190f3e68a8b69cedfa20a58
Ítalo Andrade <andraditalo@gmail.com>
2017-02-04 21:28:22 -0200

    Correção: Verificar também se lista é NodeList

--- mafiro-project/src/core/each/each.js
+++ mafiro-project/src/core/each/each.js
@@ -2,8 +2,9 @@
     mafiro.each = (array, callback) => {
         const isArray = array.constructor === Array;
         const isHTMLCollection = array.constructor === HTMLCollection;
+        const isNodeList = array.constructor === NodeList;
 
-        if (isArray || isHTMLCollection) {
+        if (isArray || isHTMLCollection || isNodeList) {
             const length = array.length;
 
             for (let i = 0; i < length; i++) {
