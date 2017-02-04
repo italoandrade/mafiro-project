2ffe6219343e162042f378e3b3c5fd2d66106b01
Ítalo Andrade <andraditalo@gmail.com>
2017-02-04 21:34:52 -0200

    Troca: Porta 80 para 8080

--- website/index.js
+++ website/index.js
@@ -34,7 +34,7 @@ if (cluster.isMaster && !debug) {
         });
     });
 
-    app.listen(80, function () {
-        console.log('Express server listening on port 80');
+    app.listen(8080, function () {
+        console.log('Express server listening on port 8080');
     });
 }
