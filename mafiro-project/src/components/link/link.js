8fee615c773b496354cfe2fc331e292bccc94241
Ítalo Andrade <andraditalo@gmail.com>
2017-02-05 22:10:54 -0200

    Implementação: Novo componente que é chamado para todos elementos de link (<a href>), faz com que todos os links sejam tratados através do Single-page Application;

---
+++ mafiro-project/src/components/link/link.js
@@ -0,0 +1,47 @@
+(() => {
+    mafiro.components.add({
+        name: 'link',
+        load: loadLink,
+        onWindowLoad: onWindowLoad
+    });
+
+    function loadLink() {
+        return {
+            on: (element) => {
+                element.addEventListener('click', (e) => {
+                    e.preventDefault();
+
+                    let href = element.getAttribute('href');
+
+                    window.history.pushState(null, 'Trackfy', href);
+
+                    if (href.substr(0, 1) === '/') {
+                        href = href.slice(1);
+                    }
+
+                    mafiro.view.load(href);
+                });
+            }
+        };
+    }
+
+    function onWindowLoad(target) {
+        console.log('- - Loading component "link"');
+
+        let $links;
+
+        if (target) {
+            $links = mafiro.element.find(target, 'a');
+        } else {
+            $links = mafiro.element('a');
+        }
+
+        mafiro.each($links, (i, $link) => {
+            if (!$link.disabled) {
+                mafiro.components.load('link').on($link);
+            }
+        });
+
+        console.log('- - Component "link" loaded');
+    }
+})();
