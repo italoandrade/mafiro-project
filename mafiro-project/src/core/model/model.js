c554c4af46ed3e4b5e78f0a1085d1815085704b4
Ítalo Andrade <andraditalo@gmail.com>
2017-02-08 21:18:16 -0200

    Pequenas correções; Implementação do mi-click; Externalização do bind em goThruBind;

--- mafiro-project/src/core/model/model.js
+++ mafiro-project/src/core/model/model.js
@@ -5,6 +5,80 @@
         this.elementReference = element;
         this.nodes = {};
 
+        goThruBinds(this);
+        goThruClicks(this);
+    }
+
+    Model.prototype.set = function (data) {
+        const dataType = typeof data;
+
+        switch (dataType) {
+            case 'object':
+                data = {
+                    vm: data
+                };
+
+                goThruModel(data, this);
+                goThruIfs(this);
+                goThruStyles(this);
+                goThruHrefs(this);
+
+                break;
+            default:
+                console.error('You\'re trying to set an incompatible type of data');
+                break;
+        }
+    };
+
+    function accessObjByPath(o, s) {
+        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+        s = s.replace(/^\./, '');           // strip a leading dot
+        var a = s.split('.');
+        for (var i = 0, n = a.length; i < n; ++i) {
+            var k = a[i];
+            if (o && k in o) {
+                o = o[k];
+            } else {
+                return;
+            }
+        }
+        return o;
+    }
+
+    function createObjByPath(o, s, value) {
+        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+        s = s.replace(/^\./, '');           // strip a leading dot
+        var a = s.split('.');
+        for (var i = 0, n = a.length; i < n; ++i) {
+            var k = a[i];
+            if (o && k in o) {
+                if (i !== n - 1) {
+                    o = o[k];
+                } else {
+                    if (value) {
+                        o[k] = value;
+                    } else {
+                        delete o[k];
+                    }
+                }
+            } else {
+                if (i !== n - 1) {
+                    o[k] = {};
+                } else {
+                    if (value) {
+                        o[k] = value;
+                    } else {
+                        delete o[k];
+                    }
+                }
+                o = o[k];
+            }
+        }
+        return o;
+    }
+
+    function goThruBinds(thisModel) {
+        const element = thisModel.elementReference;
         let elementHtml = element.innerHTML;
 
         elementHtml = elementHtml
@@ -21,78 +95,14 @@
             const textNode = document.createTextNode('');
 
             if (bind.indexOf('.') > -1) {
-                createObjByPath(this, bind, '');
-                createObjByPath(this.nodes, bind, textNode);
+                createObjByPath(thisModel.nodes, bind, textNode);
             } else {
-                this[bind] = '';
-                this.nodes[bind] = textNode;
+                thisModel[bind] = '';
+                thisModel.nodes[bind] = textNode;
             }
 
             $bind.parentNode.replaceChild(textNode, $bind);
         });
-    }
-
-    Model.prototype.set = function (data) {
-        const dataType = typeof data;
-
-        switch (dataType) {
-            case 'object':
-                data = {
-                    vm: data
-                };
-
-                goThruModel(data, this);
-
-                goThruIfs(data, this.elementReference);
-
-                goThruStyles(data, this.elementReference);
-
-                goThruHrefs(data, this.elementReference);
-
-                break;
-            default:
-                console.error('You\'re trying to set an incompatible type of data');
-                break;
-        }
-    };
-
-    function accessObjByPath(o, s) {
-        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-        s = s.replace(/^\./, '');           // strip a leading dot
-        var a = s.split('.');
-        for (var i = 0, n = a.length; i < n; ++i) {
-            var k = a[i];
-            if (k in o) {
-                o = o[k];
-            } else {
-                return;
-            }
-        }
-        return o;
-    }
-
-    function createObjByPath(o, s, value) {
-        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-        s = s.replace(/^\./, '');           // strip a leading dot
-        var a = s.split('.');
-        for (var i = 0, n = a.length; i < n; ++i) {
-            var k = a[i];
-            if (k in o) {
-                if (i !== n - 1) {
-                    o = o[k];
-                } else {
-                    o[k] = value;
-                }
-            } else {
-                if (i !== n - 1) {
-                    o[k] = {};
-                } else {
-                    o[k] = value;
-                }
-                o = o[k];
-            }
-        }
-        return o;
     }
 
     function goThruModel(data, thisModel, concat) {
@@ -115,7 +125,9 @@
         });
     }
 
-    function goThruIfs(data, $element) {
+    function goThruIfs(data) {
+        const $element = data.elementReference;
+
         const $miIfs = mafiro.element.find($element, '[mi-if]');
 
         mafiro.each($miIfs, (i, $miIf) => {
@@ -143,7 +155,9 @@
         });
     }
 
-    function goThruStyles(data, $element) {
+    function goThruStyles(data) {
+        const $element = data.elementReference;
+
         const Regex = new RegExp('(\\w{1,})(\\.(\\w{1,})){1,}', 'g');
 
         const $miStyles = mafiro.element.find($element, '[mi-style]');
@@ -179,7 +193,9 @@
         });
     }
 
-    function goThruHrefs(data, $element) {
+    function goThruHrefs(data) {
+        const $element = data.elementReference;
+
         const Regex = new RegExp('(\\w{1,})(\\.(\\w{1,})){1,}', 'g');
 
         const $miHrefs = mafiro.element.find($element, '[mi-href]');
@@ -203,5 +219,41 @@
 
             $miHref.setAttribute('href', hrefModified);
         });
+    }
+
+    function goThruClicks(data) {
+        const $element = data.elementReference;
+
+        const Regex = new RegExp('(\\w{1,})(\\.(\\w{1,})){1,}', 'g');
+
+        const $miClicks = mafiro.element.find($element, '[mi-click]');
+
+        mafiro.each($miClicks, (i, $miClick) => {
+            let click = $miClick.getAttribute('mi-click');
+
+            let clickModified = click;
+
+            const matches = click.match(Regex);
+
+            mafiro.each(matches, (o, match) => {
+                $miClick.addEventListener('click', (e) => {
+                    let value = accessObjByPath(data, match) || '';
+
+                    if (value && isFunction(value)) {
+                        value(e);
+                    } else {
+                        console.error(match + ' is not a function or doesn\'t exist');
+                    }
+                });
+            });
+
+            $miClick.setAttribute('click', clickModified);
+        });
+    }
+
+    function isFunction(functionToCheck) {
+        /*Source: http://stackoverflow.com/questions/5999998/how-can-i-check-if-a-javascript-variable-is-function-type*/
+        var getType = {};
+        return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
     }
 })();
